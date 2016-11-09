86ee723642cbccc3e3896baef16ba69647ef57d5
Henry Hu <henryhu@cpp.edu>
2016-11-09 15:59:19 -0800

    small bit of modularity, and debugging functions

--- main.rs
+++ main.rs
@@ -13,7 +13,9 @@ struct Cell {
 struct Grid {
 	rows: i32,
 	cols: i32,
-	data: Vec<Cell>
+	data: Vec<Cell>,
+	liveState: char,
+	deadState: char,
 }
 
 // Defining Cell's functions
@@ -35,11 +37,13 @@ impl Cell {
 impl Grid {
 
 	// Constructor for instantiating a new Grid
-	fn new(rows: i32, cols: i32) -> Grid {
+	fn new(rows: i32, cols: i32, live: char, dead: char) -> Grid {
 		Grid {
 			rows: rows,
 			cols: cols,
 			data: vec![],
+			liveState: live,
+			deadState: dead,
 		}
 	}
 
@@ -57,7 +61,7 @@ impl Grid {
 		for a in 0..(cols * rows) {
 			let mut index: (usize, usize) = (x, y);
 			let mut adjacentCells: Vec<usize> = self.findNeighbors(index);
-			self.data.push(Cell::new(' ', index));
+			self.data.push(Cell::new(self.deadState, index));
 			self.data[a as usize].neighbors = adjacentCells;
 
 			y += 1;
@@ -66,27 +70,6 @@ impl Grid {
 				x += 1;
 				y = 0;
 			}
-		}
-	}
-
-	// Finds a Cell's neighbors, marks them, and reveals them.
-	fn showNeighbors(&mut self, twoDIndex: (usize, usize)) {
-		let index: usize = self.getIndexConversion(twoDIndex);
-
-		let neighbors = self.data[index].neighbors.to_vec();
-		let length: usize = neighbors.len();
-
-		for a in 0..length {
-			let adjacentIndex: usize = neighbors[a];
-			self.data[adjacentIndex].state = '=';
-		}
-		self.printAllStates();
-	}
-
-	// Returns all Cells back to empty state.
-	fn clearGrid(&mut self) {
-		for a in 0..self.data.len() {
-			self.data[a].state = ' ';
 		}
 	}
 
@@ -100,6 +83,20 @@ impl Grid {
 			let adjacentIndex: usize = neighbors[a];
 			println!("{}",adjacentIndex);
 		}
+	}
+
+	// Finds a Cell's neighbors, marks them, and reveals them.
+	fn showNeighbors(&mut self, twoDIndex: (usize, usize)) {
+		let index: usize = self.getIndexConversion(twoDIndex);
+
+		let neighbors = self.data[index].neighbors.to_vec();
+		let length: usize = neighbors.len();
+
+		for a in 0..length {
+			let adjacentIndex: usize = neighbors[a];
+			self.data[adjacentIndex].state = self.liveState;
+		}
+		self.printAllStates();
 	}
 
 	// Returns a vector containing the 1d indices of the neighbors
@@ -141,11 +138,12 @@ impl Grid {
 	}
 
 	// Simulate life and death in the Grid for Cells
+	// Updats the nextLife bool
 	// Rule 1: Any live cell with < 2 live neighbors dies
 	// Rule 2: Any live cell with 2 or 3 live neighbors lives
 	// Rule 3: Any live cell with > 3 live neighbors dies
 	// Rule 4: Any dead cell with = 3 live neighbors becomes alive
-	fn live(&mut self, index: usize) {
+	fn conwaysLife(&mut self, index: usize) {
 		let neighbors = self.data[index].neighbors.to_vec();
 		let length: usize = neighbors.len();
 		let mut neighborsAlive: i32 = 0;
@@ -162,17 +160,18 @@ impl Grid {
 		if self.data[index].isAlive == true {
 			// Rule 1: Live becomes dead; under-population
 			if neighborsAlive < 2 {
-				self.data[index].isAlive = false;				
+				self.data[index].nextLife = false;				
 			} else if neighborsAlive < 4 {
 			// Rule 2: Live stays alive; next generation
+				self.data[index].nextLife = true;
 			} else {
 			// Rule 3: Live becomes dead; over-population
-				self.data[index].isAlive = false;
+				self.data[index].nextLife = false;
 			}
 		} else {
 			// Rule 4: Dead becomes alive; reproduction
 			if neighborsAlive == 3 {
-				self.data[index].isAlive = true;
+				self.data[index].nextLife = true;
 			}
 		}
 	}
@@ -201,6 +200,31 @@ impl Grid {
 		}
 	}
 
+	// All Cells in Grid become aware of their fate in the next generation
+	// If a Cell's nextLife is false, they will be represented as deadState
+	// Otherwise they will be represented as liveState
+	fn updateCells(&mut self) {
+		let total = self.cols * self.rows;
+
+		for a in 0..total {
+			if self.data[a as usize].nextLife == true {
+				self.data[a as usize].state = self.liveState;
+				self.data[a as usize].isAlive = true;
+			} else {
+				self.data[a as usize].state = self.deadState;
+				self.data[a as usize].isAlive = false;
+			}
+		}
+	}
+
+	// Returns all Cells back to dead state.
+	fn killAllCells(&mut self) {
+		for a in 0..self.data.len() {
+			self.data[a as usize].nextLife = false;
+		}
+		self.updateCells();
+	}
+
 	// A simple test render of the Grid by filling
 	// each Cell's state one at a time.
 	// Thread delay is specified in wait time
@@ -208,7 +232,7 @@ impl Grid {
 		let total = self.cols * self.rows;
 
 		for a in 0..total {
-			self.data[a as usize].state = '=';
+			self.data[a as usize].state = self.liveState;
 			self.printAllStates();
 
 			use std::{thread,time};
@@ -218,15 +242,25 @@ impl Grid {
 	}
 }
 
+// World is a 1d vector treated as a 2d vector containing Cells.
+// The Initialize function will set default values and populate World.
+// Fill the vector will Cells that will calculate their neighbor's indices.
+// Then the world will constantly run a Render function in loop forever.
+// In the loop, the World will "tick".
+// First every Cell will simultaneous check its living neighbors.
+// Conway's 4 Rules will apply to the Cells to kill or resurrect them.
+// Then the living cells will move in some way via printAllStates. Repeat.
 fn main() {
-	let mut world = Grid::new(10,10);
+	let mut world = Grid::new(10,10,'0',' ');
 	world.initialize();
 //	Testing methods below
 //	=====================
 //	world.printAllIndices();
 	world.testRender();
-	world.clearGrid();
+	world.killAllCells();
+	world.updateCells();
 	world.printAllStates();
 	world.showNeighbors((2,2));
+	world.conwaysLife(55);
 //	world.showNeighborIndices((2,2));
 }
