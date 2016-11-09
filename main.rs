cca40b59ba838382026dbb9c7957ca3d24f659a7
Henry Hu <henryhu@cpp.edu>
2016-11-08 22:31:23 -0800

    Cells now track their 2d index

--- main.rs
+++ main.rs
@@ -1,6 +1,8 @@
 // Represents Conway's cell
 struct Cell {
+	index: (usize,usize),
 	state: char,
+	neighbors: Vec<usize>,
 }
 
 // Grid is a 1d vector of size cols * rows
@@ -12,12 +14,15 @@ struct Grid {
 
 // Defining Cell's functions
 impl Cell {
-	fn new(c: char) -> Cell {
+	fn new(c: char, pair: (usize,usize)) -> Cell {
 		Cell {
+			index: pair,
 			state: c,
+			neighbors: vec![],
 		}
 	}
 
+	//TODO: May be unnecessary at the momment.
 	fn changeState(&mut self, c: char) {
 		self.state = c
 	}
@@ -35,26 +40,52 @@ impl Grid {
 
 	// Size of vector is rows * cols
 	// Initializes the vector with default values
+	// Using a for loop fill up the vector with Cells
+	// Each Cell gets their 2d array index calculated
 	fn initialize(&mut self) {
-		let total = self.cols * self.rows;
+		let cols = self.cols as usize;
+		let rows = self.rows as usize;
+		let mut x = 0 as usize;
+		let mut y = 0 as usize;
 
-		for a in 0..total {
-			self.data.push(Cell::new(' '));
+		for a in 0..(cols * rows) {
+			let mut index: (usize, usize) = (x, y);
+
+			self.data.push(Cell::new(' ', index));
+			let mut adjacentCells: Vec<usize> = vec![];
+			self.data[a as usize].neighbors = adjacentCells;
+
+			y += 1;
+
+			if y % rows == 0 {
+				x += 1;
+				y = 0;
+			}
 		}
 	}
 
 	// Method to print all states of Cells in the vector
 	// Lines separated on every 10th iteration to emulate grid
-	fn printAll(&mut self) {
+	fn printAllStates(&mut self) {
 		let total = self.cols * self.rows;
 
 		for a in 0..total {
-			if a % self.cols == 0 {
+			if a % self.rows == 0 {
 				println!("");
 			}
 			print!("[{}]",self.data[a as usize].state);
 		}
 			println!("");
+	}
+
+	// Method to print all 2d indices of the Cells in Grid
+	// Corresponding to the 1d index of Cells
+	fn printAllIndices(&mut self) {
+		let total = self.cols * self.rows;
+
+		for a in 0..total {
+			println!("[{}] = {:?}",a,self.data[a as usize].index);
+		}
 	}
 
 	// Testing render of Grid each state
@@ -63,19 +94,26 @@ impl Grid {
 		let total = self.cols * self.rows;
 
 		for a in 0..total {
-			self.data[a as usize].state = 'X';
-			self.printAll();
+			self.data[a as usize].state = '=';
+			self.printAllStates();
 
 			use std::{thread,time};
 			let wait = time::Duration::from_millis(25);
 			thread::sleep(wait);
 		}
 	}
+
+	//TODO: Retrieves a single Cell's adjacent neighbors
+	fn getCellNeighbors(&mut self, index: usize) -> Vec<usize>{
+		let a: Vec<usize> = vec![];
+		a
+	}
 }
 
 fn main() {
 	let mut world = Grid::new(10,10);
 	world.initialize();
-	world.printAll();
+	world.printAllIndices();
+	world.printAllStates();
 	world.testRender();
 }
